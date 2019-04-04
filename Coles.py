3ae7b044b9004186c40549513a7c51382428ade4
khaifahmi99 <102015077@student.swin.edu.au>
2019-04-04 23:45:52 +1100

    fix syntax error in coles.py

--- Coles.py
+++ Coles.py
@@ -25,8 +25,8 @@ def scrapping(container_soup):
             price = '$' + price_dollar.text + '.' + price_cent.text
             if(container.find('span', {'class': 'package-size'})):
                 package_size = container.find('span', {'class':'package-size'})
-                price = price + ' for ' package_size
-            if(container.find('span', {'class':'package-price'});
+                price = price + ' for ' + package_size
+            if(container.find('span', {'class':'package-price'})):
                 package_price = container.find('span', {'class':'package-price'})
                 price = price + '/' + package_price
         else:
@@ -42,38 +42,38 @@ def scrapping(container_soup):
         arr.append(obj)
     return arr
 
-    options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
-    driver = webdriver.Chrome(executable_path=r'C:\Users\ben-pchromedriver.exe',options=options)
-    arr = []
+options = webdriver.ChromeOptions()
+options.add_argument("--start-maximized")
+driver = webdriver.Chrome(executable_path=r'C:/Users/Ultabook/Downloads/chromedriver_win32/chromedriver.exe',options=options)
+arr = []
 
-    for i in range(1,8): #number of pages plus one
-        url = 'https://shop.coles.com.au/a/a-vic-metro-vermont-south/everything/browse/fruit-vegetables?pageNumber=' + str(i)
-        print('page ' + str(i) + ": " + url)
-        driver.get(url)
-        sleep(10)
-        html = driver.page_source
-        page_soup = soup(html, 'html.parser')
+for i in range(1,8): #number of pages plus one
+    url = 'https://shop.coles.com.au/a/a-vic-metro-vermont-south/everything/browse/fruit-vegetables?pageNumber=' + str(i)
+    print('page ' + str(i) + ": " + url)
+    driver.get(url)
+    sleep(10)
+    html = driver.page_source
+    page_soup = soup(html, 'html.parser')
     
-        container_soup = page_soup.findAll('div', {'class': 'product-main-info'})
-        arrSinglePage = scrapping(container_soup)
-        for obj in arrSinglePage:
-            arr.append(obj)
+    container_soup = page_soup.findAll('div', {'class': 'product-main-info'})
+    arrSinglePage = scrapping(container_soup)
+    for obj in arrSinglePage:
+        arr.append(obj)
         
-    for i in range(1,4):
-        url = 'https://shop.coles.com.au/a/a-vic-metro-vermont-south/everything/browse/fruit-vegetables?pageNumber=' + str(i)
-        print('page ' + str(i) + ": " + url)
-        driver.get(url)
-        sleep(10)
-        html = driver.page_source
-        page_soup = soup(html, 'html.parser')
+for i in range(1,4):
+    url = 'https://shop.coles.com.au/a/a-vic-metro-vermont-south/everything/browse/fruit-vegetables?pageNumber=' + str(i)
+    print('page ' + str(i) + ": " + url)
+    driver.get(url)
+    sleep(10)
+    html = driver.page_source
+    page_soup = soup(html, 'html.parser')
     
-        container_soup = page_soup.findAll('div', {'class': 'product-main-info'})
-        arrSinglePage = scrapping(container_soup)
-        for obj in arrSinglePage:
-            arr.append(obj)    
+    container_soup = page_soup.findAll('div', {'class': 'product-main-info'})
+    arrSinglePage = scrapping(container_soup)
+    for obj in arrSinglePage:
+        arr.append(obj)    
     
-    with open('colesData.json', 'w') as outfile:
-        json.dump(arr, outfile)
+with open('colesData.json', 'w') as outfile:
+    json.dump(arr, outfile)
 
-    print(len(arr))
+print(len(arr))
